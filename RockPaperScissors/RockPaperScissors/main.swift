ded15833130282e8fc58de6f0dbdc5d5efb104fb
ijs1103 <ajaijs1103@naver.com>
2023-11-22 13:27:30 +0900

    feat(printChoice 함수): 유저에게 입력사항을 표시

--- RockPaperScissors/RockPaperScissors/main.swift
+++ RockPaperScissors/RockPaperScissors/main.swift
@@ -1,10 +1,12 @@
 //
-//  RockPaperScissors - main.swift
-//  Created by tacocat.
-//  Copyright © tastycode. All rights reserved.
-// 
+//  main.swift
+//  RockPaperScissors
+//
+//  Created by 이주상 on 2023/11/22.
+//
 
 import Foundation
 
-print("Hello, World!")
-
+private func printChoice() {
+    print("가위(1), 바위(2), 보(3)! <종료 : 0> :")
+}
